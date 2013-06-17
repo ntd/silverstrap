6d65f7f704c204b90354a72d5038feb02a3cd561
Nicola Fontana <ntd@entidi.it>
2013-06-17 14:27:27 +0200

    Allow custom image URLs
    Reapplied #0ab3b5c5dc996dee: colorbox by default detects if an URL is
    an image by checking if the ending part is a common image extension
    (.jpeg, .png etc...). This clashes with the custom URLs used by
    silverstripe-adg to dynamically generate the technical drawing, so
    that checking is now disabled.

--- js/silverstrap.js
+++ js/silverstrap.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     // Add ColorBox support for zooming images, if required
     if ($.isFunction($.colorbox)) {
-	$('.zoom').colorbox({ rel: 'group' });
+	$('.zoom').colorbox({
+	    rel: 'group',
+	    photo: true // Do not rely on the URL for detecting an image
+	});
     }
 
     // Customize the behavior of the table of contents, if it exists
